1ac20caa91d0eccd504811479b6fb72cc03a12ea
Stefan Carpeliuc <birkavidit@gmail.com>
2026-07-16 17:31:59 +0200

    working fractal with some amount of scaling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 mod learn_opengl;
 
+use std::ffi::CString;
+
 use beryllium::{
-  events::Event,
-  init::InitFlags,
-  video::{CreateWinArgs, GlContextFlags, GlProfile, GlSwapInterval},
-  *,
+  events::{Event, SDLK_DOWN, SDLK_ESCAPE, SDLK_LEFT, SDLK_RIGHT, SDLK_UP, SDLK_x, SDLK_z}, video::GlSwapInterval, *,
 };
 use ogl33::*;
 use learn_opengl::{
@@ -18,25 +17,71 @@ use learn_opengl::{
 
 const WINDOW_TITLE: &str = "Fractals";
 type Vertex = [f32; 3];
-type TriIndexes = [u32; 3];
+// type TriIndexes = [u32; 3];
 
 
-const VERTICES: [Vertex; 4] =
-  [[0.5, 0.5, 0.0], [0.5, -0.5, 0.0], [-0.5, -0.5, 0.0], [-0.5, 0.5, 0.0]];
+// const VERTICES: [Vertex; 4] =
+//   [[0.5, 0.5, 0.0], [0.5, -0.5, 0.0], [-0.5, -0.5, 0.0], [-0.5, 0.5, 0.0]];
+const VERTICES: [Vertex; 3] =
+  [[-1.0, -1.0, 0.0], [-1.0, 3.0, 0.0], [3.0, -1.0, 0.0]];
 
-const INDICES: [TriIndexes; 2] = [[0, 1, 3], [1, 2, 3]];
+// const INDICES: [TriIndexes; 2] = [[0, 1, 3], [1, 2, 3]];
 
 const VERT_SHADER: &str = r#"#version 330 core
   layout (location = 0) in vec3 pos;
+  out vec2 uv;
+
   void main() {
     gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
+    uv = (pos * 0.5 + 0.5).xy;
   }
 "#;
 const FRAG_SHADER: &str = r#"#version 330 core
+  in vec2 uv;
   out vec4 final_color;
+  uniform float zoom;
+  uniform vec2 offset;
+
+  float map(float value, float oldMin, float oldMax,
+            float newMin, float newMax)
+  {
+    return newMin +
+           (value - oldMin) * (newMax - newMin) /
+           (oldMax - oldMin);
+  }
+
+  // vec2 where x is number and y is 10 to the power
 
   void main() {
-    final_color = vec4(1.0, 0.5, 0.2, 1.0);
+    int max_iterations = 100;
+
+    int n = 0;
+    float a = map(uv.x, 0, 1, -2 * zoom, 2 * zoom);
+    float b = map(uv.y, 0, 1, -2 * zoom, 2 * zoom); 
+
+    a += offset.x;
+    b += offset.y;
+
+    float orig_a = a;
+    float orig_b = b;
+    while (n < max_iterations) {
+      float next_a = a * a - b * b;
+      float next_b = 2 * a * b;
+      a = next_a + orig_a;
+      b = next_b + orig_b;
+
+      if(abs(a+b) > 16) { break; }
+
+      n++;
+    }
+    float bright = map(n, 0, max_iterations, 0, 1);
+    if(n == max_iterations) bright = 0;
+
+    final_color = vec4(bright, bright, 0.2, 1.0);
+  }
+
+  vec2 mathh(vec2 a, vec2 b) {
+
   }
 "#;
 
@@ -77,7 +122,7 @@ fn main() {
   let vao = VertexArray::new().expect("Couldn't make vao");
   vao.bind();
 
-  let mut vbo = Buffer::new().expect("Couldn't make vbo");
+  let vbo = Buffer::new().expect("Couldn't make vbo");
   vbo.bind(BufferType::Array);
   learn::buffer_data(
     BufferType::Array, 
@@ -85,13 +130,13 @@ fn main() {
     GL_STATIC_DRAW
   );
 
-  let mut ebo = Buffer::new().expect("Couldn't make ebo");
-  ebo.bind(BufferType::ElementArray);
-  learn::buffer_data(
-    BufferType::ElementArray, 
-    bytemuck::cast_slice(&INDICES),
-    GL_STATIC_DRAW
-  );
+  // let mut ebo = Buffer::new().expect("Couldn't make ebo");
+  // ebo.bind(BufferType::ElementArray);
+  // learn::buffer_data(
+  //   BufferType::ElementArray, 
+  //   bytemuck::cast_slice(&INDICES),
+  //   GL_STATIC_DRAW
+  // );
 
   unsafe {
     glVertexAttribPointer(
@@ -109,20 +154,65 @@ fn main() {
 
   learn::polygon_mode(learn::PolygonMode::Fill);
 
+  let mut zoom = 1.0;
+  let zoom_strength = 0.9;
+  let mut pos = (0.0,0.0);
+  let move_speed = 0.05;
+
+  let zoom_name = CString::new("zoom").unwrap();
+  let zoom_location = unsafe {
+    glGetUniformLocation(programm.0, zoom_name.as_ptr())
+  };
+
+  let pos_name = CString::new("offset").unwrap();
+  let pos_location = unsafe {
+    glGetUniformLocation(programm.0, pos_name.as_ptr())
+  };
+  
   'main_loop: loop {
-    while let Some(event) = sdl.poll_events() {
+    while let Some((event, _remaining)) = sdl.poll_events() {
       match event {
-          (events::Event::Quit, _) => break 'main_loop,
-          _ => (),
+        Event::Quit => break 'main_loop,
+        Event::Key { pressed, keycode, .. } => {
+          if pressed {
+            match keycode {
+              SDLK_x => {
+                  zoom *= zoom_strength;
+              }
+              SDLK_z => {
+                  zoom /= zoom_strength;
+              }
+              SDLK_DOWN => {
+                pos.1 -= move_speed * zoom;
+              }
+              SDLK_UP => {
+                pos.1 += move_speed * zoom;
+              }
+              SDLK_RIGHT => {
+                pos.0 += move_speed * zoom;
+              }
+              SDLK_LEFT => {
+                pos.0 -= move_speed * zoom;
+              }
+              SDLK_ESCAPE => {
+                  break 'main_loop;
+              }
+              _ => {}
+            }
+          }
+        }
+        _ => (),
       }
     }
     // now the events are clear
     // here's where we could change the world state and draw.
     unsafe {
       glClear(GL_COLOR_BUFFER_BIT);
-      glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 as *const _);
+      // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 as *const _);
+      glDrawArrays(GL_TRIANGLES, 0, 3);
+      glUniform1f(zoom_location, zoom);
+      glUniform2f(pos_location, pos.0, pos.1);
       win.swap_window();
     }
   }
 }
-
